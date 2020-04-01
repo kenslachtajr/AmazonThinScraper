8b35d1be719a8fa89ceed1d380ef019b37517d13
Kenneth K. Slachta, Jr <ken.slachta@gmail.com>
2020-04-01 15:19:27 -0700

    initial commit

---
+++ spiders/AmazonProductSpider.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from ..items import AmazonItem
+
+
+class AmazonProductSpider(scrapy.Spider):
+    name = "AmazonDeals"
+    allowed_domains = ["amazon.com"]
+
+    # Use working product URL below
+    start_urls = [
+        "https://www.amazon.com/dp/B00JM5GZGW",
+        "https://www.amazon.com/dp/B01MZ9E6CG",
+        "https://www.amazon.com/dp/B003HGGPLW"
+    ]
+
+    def parse(self, response):
+         items = AmazonItem()
+         title = response.xpath('//h1[@id="title"]/span/text()').extract()
+         sale_price = response.xpath('//span[contains(@id,"ourprice") or contains(@id,"saleprice")]/text()').extract()
+         category = response.xpath('//a[@class="a-link-normal a-color-tertiary"]/text()').extract()
+         availability = response.xpath(
+         '//div[@id="availability"]//text()').extract()
+         items['product_name'] = ''.join(title).strip()
+         items['product_sale_price'] = ''.join(sale_price).strip()
+         items['product_category'] = ','.join(map(lambda x: x.strip(), category)).strip()
+         items['product_availability'] = ''.join(availability).strip()
+         yield items
